24fc448b1efefb7722665b1788b87242a3872d10
sagunmahrn <sagunmaharjan68@gmail.com>
2023-05-06 12:49:57 +0545

    add new file

--- cs50/hello.py
+++ cs50/hello.py
@@ -1,7 +1,11 @@
 # Ask user for their name
 name = input("what's your name?\n ").strip().title()
 
+# Split user's name into first name and last name
+# first, last = name.split(" ") 
+
 # say hello to user
-print(f"hello,{ name}")
+print(f"hello,{name}")
+# print(f"hello,{first}")
 
 
