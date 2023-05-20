9d1b20a0e97b5d8ae7263217c07c7be04e9e24f2
sagunmahrn <sagunmaharjan68@gmail.com>
2023-05-20 23:49:06 +0545

    added loop statement

--- cs50/exception_hello.py
+++ cs50/exception_hello.py
@@ -1,8 +1,12 @@
 # print("hello,world")
 
-try:
-    x = int(input("What's X?"))    
-except ValueError:
-    print("x is not an integer")
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = int(input("What's X?"))    
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break  
+        # if interger value is inserted then break is executed
+    
+print(f"x is {x}")
