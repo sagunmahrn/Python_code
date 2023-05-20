b994e3d5af42e6afc4a798fb9437fd18005f5f92
sagunmahrn <sagunmaharjan68@gmail.com>
2023-05-21 00:05:22 +0545

    add function argument

--- cs50/exception_hello.py
+++ cs50/exception_hello.py
@@ -1,15 +1,12 @@
 # print("hello,world")
 def main():
-    x=get_int()
+    x=get_int("What's x?")
     print(f"x is {x}")
 
-
-
-
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            return int(input("What's X?"))    
+            return int(input(prompt))    
         except ValueError:
             # print("x is not an integer")
             pass
